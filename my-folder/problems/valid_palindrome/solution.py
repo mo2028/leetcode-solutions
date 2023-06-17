4a33faa55c2f3e92d4f9c85745288de28ac13346
Mohith Kankanala <mohithkankanalaks@gmail.com>
2023-06-17 22:05:52 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- my-folder/problems/valid_palindrome/solution.py
+++ my-folder/problems/valid_palindrome/solution.py
@@ -1,25 +1,38 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #covert to have only alphanumeric characters
-        l, r = 0, len(s) -1
+        # 2. init left and right pointers 
+        l, r = 0, len(s)-1
 
-        while l <= r:
-            while l < r and not self.alphaNum(s[l]):
+        # forgot the main l<r loop that drives the whole thing
+        while l < r:
+            # 4. while loops to forward or backwads the pointer if not alphanumeric
+            # forgot the self.isAlphanumeric and the l < r
+            while l < r and not self.isAlphaNumeric(s[l]):
                 l += 1
-            while r > l and not self.alphaNum(s[r]): 
-                r -= 1   
-
+            while l < r and not self.isAlphaNumeric(s[r]):
+                r -= 1
+        
+            # 3. while loop to check if the left and right character are the same
+            # what is .lower?
+            # I had "while s[l].lower() != s[r].lower():", it it just a check once
             if s[l].lower() != s[r].lower():
                 return False
-            l, r = l +1, r -1
+        
+            # 5. update forward and bakcward pointers
+            # if the program reached here it means that it is okay to update
+     
+            l += 1
+            r -= 1
+
+        # if it gets out of the loop successfully then return true
         return True
 
-    def alphaNum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9')
-                                            )
 
 
 
-        
+    # 1. first  write a function to check if a character is alpha numeric
+    # had this indented inside the isPalindrome function, which is wrong becuase it is a class method of Solution. 
+    def isAlphaNumeric(self, c):
+        return (ord("A") <= ord(c) <= ord("Z") or 
+                ord("a") <= ord(c) <= ord("z") or
+                ord("0") <= ord(c) <= ord("9"))
