0270d89de8638e38172e370a05e0338c7b937ba0
Mohith Kankanala <mohithkankanalaks@gmail.com>
2023-06-25 00:28:11 +0000

    Sync LeetCode submission - 3Sum (python3)

--- my-folder/problems/3sum/solution.py
+++ my-folder/problems/3sum/solution.py
@@ -1,30 +1,55 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()  # Sort the input list in ascending order
+        # sort the numbers
+        nums.sort()
+
+        # init result list
         result = []
-        
+
+
+        # outer for loop that costs O(n)
         for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Skip duplicate values for i to avoid duplicates in the result
-            
-            left = i + 1
-            right = len(nums) - 1
-            
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
+
+
+            # skip duplicate of i 
+            # forgot about this if statement and handeling the case of the dulicates of i
+            if i > 0 and nums[i] == nums[i-1]:
+                continue 
+
+
+            # init left and right 
+            l = i + 1
+            r = len(nums) - 1
+
+
+            # while loop 2 sum
+            while l < r:
+                threeSum = nums[i] + nums[l] + nums[r]
+
                 
-                if total < 0:
-                    left += 1
-                elif total > 0:
-                    right -= 1
+                # if statements to move the pointers
+                if threeSum < 0:
+                    l+=1
+                elif threeSum > 0:
+                    r-=1
                 else:
-                    result.append([nums[i], nums[left], nums[right]])
-                    # Skip duplicate values for left and right to avoid duplicates in the result
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-        
-        return result
+                    result.append([nums[i], nums[l], nums[r]])
+
+                    # while loops to make sure you skip the dupicated
+                    # the below while loops were outsie of the else before, and think about why it has to be inside the else statement 
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+
+
+                    # i had a quesiton about whether to include the below updation
+                    # the below code is absolutely needed because, the while loops only execute if there are duplicates and if there are no duplicates, 
+                    # we still need to update it using the code below
+                    l += 1
+                    r -= 1
+
+        return result       
+
+            
