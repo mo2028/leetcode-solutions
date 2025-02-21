fe9f3002d1303e1de0573859862f40bd07b2fb7f
Mohith Kankanala <mohithkankanalaks@gmail.com>
2025-02-21 04:48:53 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- my-folder/problems/valid_anagram/solution.py
+++ my-folder/problems/valid_anagram/solution.py
@@ -2,18 +2,13 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
-        my_dict = {}
-        anagram_dict = {}
-        
-        for alpha in s:
-            my_dict[alpha] = my_dict.get(alpha, 0) + 1
 
-        for alpha in t:
-            anagram_dict[alpha] = anagram_dict.get(alpha, 0) + 1
-
-        for alpha in t:
-            if anagram_dict.get(alpha, 0) != my_dict.get(alpha, 0):
+        count = [0] *26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for n in count:
+            if n != 0:
                 return False
-        
         return True
