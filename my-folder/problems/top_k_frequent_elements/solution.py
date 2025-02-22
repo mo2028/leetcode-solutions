ad5991afadd1fc2a4f2c984232d5bde88d3e68de
Mohith Kankanala <mohithkankanalaks@gmail.com>
2025-02-22 03:07:26 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- my-folder/problems/top_k_frequent_elements/solution.py
+++ my-folder/problems/top_k_frequent_elements/solution.py
@@ -1,30 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        # list of lists
-        # forgot the keyword "range"
-        freq = [[] for i in range(len(nums)+1)]
-
-        # count the frequences of n, using a map {n -> count}
-        for n in nums:
-            # have to use the .get method for the 0 as defualt value
-            # don't need to do "count[n].get(n , 0)" because n is a param
-            count[n] = count.get(n , 0) + 1
-        
-        # for each index in the freq list, place the n,
-        #  where its count == freq[ index]
-        # not count.values(), its count.items()
-        for n, c in count.items():
-            freq[c].append(n)
-
-        
         res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
+        freq = [[] for i in range(len(nums)+1)]
+        count = {}
+        for i in range(len(nums)):
+            count[nums[i]] = count.get(nums[i], 0)+1
+        # print(count)
+
+        for n, cnt in count.items():
+            freq[cnt].append(n)
+
+        for i in range(len(freq)-1, 0, -1):
+            for u in freq[i]:
+                res.append(u)
                 if len(res) == k:
                     return res
-
 
 
         
