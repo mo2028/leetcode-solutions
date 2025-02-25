5347333a82ccbcbefe154a7f51fe8b568909f09b
Mohith Kankanala <mohithkankanalaks@gmail.com>
2025-02-25 01:09:50 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- my-folder/problems/top_k_frequent_elements/solution.py
+++ my-folder/problems/top_k_frequent_elements/solution.py
@@ -1,20 +1,36 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        res = []
+        # will solve this using bucket sort
+        # index of the freq list is the number of times that number in the list appears
         freq = [[] for i in range(len(nums)+1)]
-        count = {}
+        # get the count of all the numbers to be out in the freq buckets
+        countMap = {}
         for i in range(len(nums)):
-            count[nums[i]] = count.get(nums[i], 0)+1
-        # print(count)
+            countMap[nums[i]] = countMap.get(nums[i], 0) + 1
+        print(countMap)
+        
+        # put it into the buckets, where the index is the count
+        for number, count in countMap.items():
+            # print(number, count)
+            freq[count].append(number)
+                
 
-        for n, cnt in count.items():
-            freq[cnt].append(n)
-
-        for i in range(len(freq)-1, 0, -1):
-            for u in freq[i]:
-                res.append(u)
+        res = []
+        # print(freq)
+        # go from the back and return k elements
+        for i in range(len(nums), 0, -1):
+            for j in freq[i]:
+                res.append(j)
                 if len(res) == k:
                     return res
+
+
+
+
+
+
+
+        
 
 
         
