aa0a1d405bb6881d02bd8f5cd813805443a52fa6
Mohith Kankanala <mohithkankanalaks@gmail.com>
2025-02-27 03:05:42 +0000

    Sync LeetCode submission - 3Sum (python3)

--- my-folder/problems/3sum/solution.py
+++ my-folder/problems/3sum/solution.py
@@ -1,55 +1,35 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # sort the numbers
+        res = []
         nums.sort()
 
-        # init result list
-        result = []
-
-
-        # outer for loop that costs O(n)
-        for i in range(len(nums) - 2):
-
-
-            # skip duplicate of i 
-            # forgot about this if statement and handeling the case of the dulicates of i
-            if i > 0 and nums[i] == nums[i-1]:
-                continue 
-
-
-            # init left and right 
-            l = i + 1
-            r = len(nums) - 1
-
-
-            # while loop 2 sum
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+            # skip duplicates
+            if i > 0 and a == nums[i-1]:
+                continue
+            
+            l, r = i+1, len(nums) -1
             while l < r:
-                threeSum = nums[i] + nums[l] + nums[r]
+                threeSum = a + nums[l] + nums[r]
+
+                if threeSum > 0:
+                    r -=1
+                elif threeSum < 0:
+                    l +=1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    r-=1
+                    l+=1
+
+                    while nums[l] == nums[l-1] and l < r:
+                        l+=1
+                    while nums[r] == nums[r+1] and l < r:
+                        r-=1
+        return res
 
                 
-                # if statements to move the pointers
-                if threeSum < 0:
-                    l+=1
-                elif threeSum > 0:
-                    r-=1
-                else:
-                    result.append([nums[i], nums[l], nums[r]])
-
-                    # while loops to make sure you skip the dupicated
-                    # the below while loops were outsie of the else before, and think about why it has to be inside the else statement 
-                    while l < r and nums[l] == nums[l+1]:
-                        l += 1
-
-                    while l < r and nums[r] == nums[r-1]:
-                        r -= 1
 
 
-                    # i had a quesiton about whether to include the below updation
-                    # the below code is absolutely needed because, the while loops only execute if there are duplicates and if there are no duplicates, 
-                    # we still need to update it using the code below
-                    l += 1
-                    r -= 1
-
-        return result       
-
-            
+        
