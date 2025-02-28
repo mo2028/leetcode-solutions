16b65057bd59bbe080ac48fa101202db5a65b7b4
Mohith Kankanala <mohithkankanalaks@gmail.com>
2025-02-28 02:36:27 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (python3)

--- my-folder/problems/two_sum_ii_-_input_array_is_sorted/solution.py
+++ my-folder/problems/two_sum_ii_-_input_array_is_sorted/solution.py
@@ -1,26 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # 1. init left and right pointers
-        l, r = 0, len(numbers)-1
+        l, r = 0, len(numbers) -1
 
-        # 2. big driver loop
         while l < r:
-
-            tot = numbers[l] + numbers[r]
-
-            # if statements to update the l and r pointers
-            if tot  < target:
-                l += 1
-            elif tot > target:
-                r -= 1
+            currSum = numbers[l] + numbers[r]
+            if currSum < target:
+                l+=1
+            elif currSum > target:
+                r-=1
             else:
                 return [l+1, r+1]
-                
-            
-
-
-
-
-
-
-
+        
