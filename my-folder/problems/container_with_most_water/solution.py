7a286aaefa6d96718dc2f183d215917a32b993f5
Mohith Kankanala <mohithkankanalaks@gmail.com>
2025-03-07 05:16:41 +0000

    Sync LeetCode submission - Container With Most Water (python3)

--- my-folder/problems/container_with_most_water/solution.py
+++ my-folder/problems/container_with_most_water/solution.py
@@ -1,35 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # 1 init left and right pointer
-        left = 0
-        right = len(height) -1
+        maxArea = 0
+        l,r = 0, len(height)-1
 
-        maxArea = -1
-        # 3 while loop to go throught all the values
-        while left < right:
-            # 2 calculate area
-            # I had "area = max(height[left], height[right]) * (right - left)", which is wrong because it should be min
-            area = min(height[left], height[right]) * (right - left)
-            # 4 if statement for updating left 
-            if height[left] < height[right]:
-                left += 1
-            # 5 if statement for updating right 
+        while l < r:
+            area = (r-l) * min(height[l], height[r])
+            maxArea = max(area, maxArea)
+
+            if height[l] <= height[r]:
+                l+=1
             else:
-                right -= 1
-            # max area
-            maxArea = max(maxArea, area)
-
-        return maxArea
-
-
-
+                r-=1
         
 
-
-
-
-
-
-
-
-       
+        return maxArea
+        
