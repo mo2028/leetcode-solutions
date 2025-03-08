8a29843f3a5b9b42b112d0b3273c8426a4196488
Mohith Kankanala <mohithkankanalaks@gmail.com>
2025-03-08 05:28:39 +0000

    Sync LeetCode submission - 3Sum (python3)

--- my-folder/problems/3sum/solution.py
+++ my-folder/problems/3sum/solution.py
@@ -3,33 +3,31 @@ class Solution:
         res = []
         nums.sort()
 
-        for i, a in enumerate(nums):
-            if a > 0:
-                break
-            # skip duplicates
-            if i > 0 and a == nums[i-1]:
+        for i, n in enumerate(nums):
+            if nums[0] > 0:
+                return res
+            # skip same ones, but make sure you are not going out of bounds error
+            # so check i > 0 so that nums[i-1] never errors out
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
-            
-            l, r = i+1, len(nums) -1
+
+            l, r = i+1, len(nums)-1
+
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-
-                if threeSum > 0:
+                total = nums[i] + nums[l] + nums[r]
+                if total < 0:
+                    l += 1
+                elif total > 0:
                     r -=1
-                elif threeSum < 0:
-                    l +=1
                 else:
-                    res.append([a, nums[l], nums[r]])
-                    r-=1
+                    res.append([nums[i], nums[l], nums[r]])
                     l+=1
+                    r-=1
 
-                    while nums[l] == nums[l-1] and l < r:
+                    while l < r and nums[l-1] == nums[l]:
                         l+=1
-                    while nums[r] == nums[r+1] and l < r:
+                    while l < r and nums[r+1] == nums[r]:
                         r-=1
         return res
 
-                
-
-
-        
+       
